94fa90e2f2c828175edd4fefbe1eeed612846d9f
Richard Pineo <Richard.E.Pineo@ge.com>
2018-12-02 22:15:05 -0800

    2b solved

--- all-puzzles.ts
+++ all-puzzles.ts
@@ -2,8 +2,10 @@ import Puzzle from './src/puzzle/puzzle'
 import Puzzle1a from './src/puzzle/puzzle-1a';
 import Puzzle1b from './src/puzzle/puzzle-1b';
 import Puzzle2a from './src/puzzle/puzzle-2a';
+import Puzzle2b from './src/puzzle/puzzle-2b';
 
 export const puzzles = Array<Puzzle>();
 puzzles.push(new Puzzle1a());
 puzzles.push(new Puzzle1b());
 puzzles.push(new Puzzle2a());
+puzzles.push(new Puzzle2b());
