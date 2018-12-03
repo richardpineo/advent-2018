5019588f0aba1f7d3e3520bf56d0d3dbcf142e5c
Richard Pineo <Richard.E.Pineo@ge.com>
2018-12-02 22:02:08 -0800

    solve 2a

--- src/puzzle/puzzle-2a.ts
+++ src/puzzle/puzzle-2a.ts
@@ -1,6 +1,8 @@
 
 import Puzzle from './puzzle'
 
+import * as _ from 'lodash'
+
 export default class Puzzle2a extends Puzzle {
     constructor() {
         super("2a: Checksum");
@@ -8,10 +10,26 @@ export default class Puzzle2a extends Puzzle {
 
     solve() {
         const lines = this.readLines('./data/2');
+        let hasTwice = 0;
+        let hasThrice = 0;
         lines.forEach(v => {
-            console.log(v);
+            let lineHasTwice = false;
+            let lineHasThrice = false;
+            const grouped = _.groupBy(v);
+            for (const letter in grouped) {
+                switch (grouped[letter].length) {
+                    case 2:
+                        lineHasTwice = true;
+                        break;
+                    case 3:
+                        lineHasThrice = true;
+                        break;
+                }
+            }
+            hasTwice += (lineHasTwice ? 1 : 0);
+            hasThrice += (lineHasThrice ? 1 : 0);
         })
 
-        //        console.log(`Value is ${value}`);
+        console.log(`Checksum is ${hasTwice * hasThrice} (${hasTwice} and ${hasThrice})`);
     }
 }
