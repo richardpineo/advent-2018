c2c35f78d706a7041362f19bfeadbfe57f7f9a22
Richard Pineo <Richard.E.Pineo@ge.com>
2018-12-12 19:44:05 -0800

    genericized

--- src/puzzle/puzzle-11.ts
+++ src/puzzle/puzzle-11.ts
@@ -1,6 +1,32 @@
 
 import Puzzle from './puzzle'
 
+class Grid {
+    constructor(public size: number, public serial: number) {
+    }
+
+    powerFor(x: number, y: number): number {
+        return this.powerLevel(x, y) +
+            this.powerLevel(x + 1, y) +
+            this.powerLevel(x + 2, y) +
+            this.powerLevel(x, y + 1) +
+            this.powerLevel(x + 1, y + 1) +
+            this.powerLevel(x + 2, y + 1) +
+            this.powerLevel(x, y + 2) +
+            this.powerLevel(x + 1, y + 2) +
+            this.powerLevel(x + 2, y + 2);
+    }
+
+    powerLevel(x: number, y: number): number {
+        const rackId = x + 10;
+        let powerLevel = y * rackId;
+        powerLevel += this.serial;
+        powerLevel *= rackId;
+        powerLevel = Math.floor((powerLevel % 1000) / 100);
+        powerLevel -= 5;
+        return powerLevel;
+    }
+}
 
 // 11a: Max power is 30 at (21, 61)
 // 11b:
@@ -16,7 +42,7 @@ export default class Puzzle11 extends Puzzle {
     }
 
     solveA() {
-        const grid = this.gridForSerialNumber(6042);
+        const grid = new Grid(300, 6042);
         const power = this.maxPower(grid);
         console.log(`11a: Max power is ${power[2]} at (${power[0]}, ${power[1]})`);
     }
@@ -26,33 +52,33 @@ export default class Puzzle11 extends Puzzle {
     }
 
     runTestCases() {
-        const grid1 = this.gridForSerialNumber(8);
+        const grid1 = new Grid(300, 8);
         this.checkValue(grid1, 3, 5, 4);
 
-        const grid2 = this.gridForSerialNumber(57);
+        const grid2 = new Grid(300, 57);
         this.checkValue(grid2, 122, 79, -5);
 
-        const grid3 = this.gridForSerialNumber(39);
+        const grid3 = new Grid(300, 39);
         this.checkValue(grid3, 217, 196, 0);
 
-        const grid4 = this.gridForSerialNumber(71);
+        const grid4 = new Grid(300, 71);
         this.checkValue(grid4, 101, 153, 4);
 
-        const grid5 = this.gridForSerialNumber(18);
+        const grid5 = new Grid(300, 18);
         this.checkPower(grid5, 33, 45, 29);
 
-        const grid6 = this.gridForSerialNumber(42);
+        const grid6 = new Grid(300, 42);
         this.checkPower(grid6, 21, 61, 30);
     }
 
-    checkValue(grid: number[], x: number, y: number, expected: number) {
-        const value = grid[this.index(x, y)];
+    checkValue(grid: Grid, x: number, y: number, expected: number) {
+        const value = grid.powerLevel(x, y);
         if (value !== expected) {
             console.log(`(${x},${y}) is wrong! expected ${expected} but was ${value}`);
         }
     }
 
-    checkPower(grid: number[], x: number, y: number, expected: number) {
+    checkPower(grid: Grid, x: number, y: number, expected: number) {
         const power = this.maxPower(grid);
         if (power[0] != x || power[1] != y || power[2] != expected) {
             console.log(`Power wrong! Expected ${expected} at (${x}, ${y}) but got ${power.toString()}`);
@@ -60,11 +86,11 @@ export default class Puzzle11 extends Puzzle {
     }
 
     // 3 array elements - x,y,power
-    maxPower(grid: number[]): number[] {
+    maxPower(grid: Grid): number[] {
         let max = [-1, -1, -10];
-        for (let y = 0; y < 298; y++) {
-            for (let x = 0; x < 298; x++) {
-                const power = this.powerFor(grid, x, y);
+        for (let y = 0; y < grid.size - 2; y++) {
+            for (let x = 0; x < grid.size - 2; x++) {
+                const power = grid.powerFor(x, y);
                 if (power > max[2]) {
                     max[0] = x;
                     max[1] = y;
@@ -73,41 +99,5 @@ export default class Puzzle11 extends Puzzle {
             }
         }
         return max;
-    }
-
-    powerFor(grid: number[], x: number, y: number): number {
-        return grid[this.index(x, y)] +
-            grid[this.index(x + 1, y)] +
-            grid[this.index(x + 2, y)] +
-            grid[this.index(x, y + 1)] +
-            grid[this.index(x + 1, y + 1)] +
-            grid[this.index(x + 2, y + 1)] +
-            grid[this.index(x, y + 2)] +
-            grid[this.index(x + 1, y + 2)] +
-            grid[this.index(x + 2, y + 2)];
-    }
-
-    gridForSerialNumber(serialNumber: number): number[] {
-        let powerLevels = Array<number>(90000);
-        for (let y = 0; y < 300; y++) {
-            for (let x = 0; x < 300; x++) {
-                powerLevels[this.index(x, y)] = this.powerLevel(x, y, serialNumber);
-            }
-        }
-        return powerLevels;
-    }
-
-    index(x: number, y: number): number {
-        return y * 300 + x;
-    }
-
-    powerLevel(x: number, y: number, serialNumber: number) {
-        const rackId = x + 10;
-        let powerLevel = y * rackId;
-        powerLevel += serialNumber;
-        powerLevel *= rackId;
-        powerLevel = Math.floor((powerLevel % 1000) / 100);
-        powerLevel -= 5;
-        return powerLevel;
     }
 }
