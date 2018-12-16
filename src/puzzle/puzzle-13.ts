aaa2479c8b373ccceba0395c1a199746f645247c
Richard Pineo <Richard.E.Pineo@ge.com>
2018-12-16 14:38:39 -0800

    good start

--- src/puzzle/puzzle-13.ts
+++ src/puzzle/puzzle-13.ts
@@ -16,6 +16,82 @@ class Location {
 class Cart {
 	constructor(public location: Location, public facing: Direction) {
 	}
+
+	private turnCount = 0;
+
+	moveOrder(track: Track) {
+		return this.location.x * track.dimX + this.location.y;
+	}
+
+	turnRight() {
+		switch (this.facing) {
+			case Direction.North:
+				this.facing = Direction.East;
+			case Direction.East:
+				this.facing = Direction.South;
+			case Direction.South:
+				this.facing = Direction.West;
+			case Direction.West:
+				this.facing = Direction.North;
+		}
+	}
+
+	turnLeft() {
+		switch (this.facing) {
+			case Direction.North:
+				this.facing = Direction.West;
+			case Direction.West:
+				this.facing = Direction.South;
+			case Direction.South:
+				this.facing = Direction.East;
+			case Direction.East:
+				this.facing = Direction.North;
+		}
+	}
+
+	advance() {
+		switch (this.facing) {
+			case Direction.North:
+				this.location.y += -1;
+				break;
+			case Direction.South:
+				this.location.y += 1;
+				break;
+			case Direction.West:
+				this.location.x += -1;
+				break;
+			case Direction.East:
+				this.location.x += 1;
+				break;
+		}
+	}
+
+	move(track: Track) {
+		this.advance();
+
+		const part = track.getPart(this.location);
+		switch (part) {
+			case TrackPart.Clockwise:
+				this.turnRight();
+				break;
+			case TrackPart.Widdershins:
+				this.turnLeft();
+				break;
+			case TrackPart.Intersection: {
+				const action = this.turnCount % 3;
+				switch (action) {
+					case 0:
+						this.turnLeft();
+						break;
+					case 1:
+					case 2:
+						this.turnRight();
+						break;
+				}
+				this.turnCount++;
+			}
+		}
+	}
 }
 
 enum TrackPart {
@@ -57,10 +133,44 @@ export default class Puzzle12 extends Puzzle {
 	}
 
 	private solveA() {
-		console.log(`42`);
+		const collision = this.runUntilCollision();
+		if (collision === undefined) {
+			console.log(`No collision found!`.red);
+		}
+		else {
+			console.log(`${collision.x},${collision.y} collision detected`)
+		}
 	}
 
 	private solveB() {
+	}
+
+	private runUntilCollision(): Location | undefined {
+		for (let tick = 0; tick < 10000; tick++) {
+			// Sort the carts by move order
+			this.carts.sort((a, b) => a.moveOrder(this.track) - b.moveOrder(this.track));
+			for (let i = 0; i < this.carts.length; i++) {
+				const c = this.carts[i];
+
+				c.move(this.track);
+
+				// check for collision
+				const collision = this.hasCollisionAt();
+				if (collision !== undefined) {
+					return collision;
+				}
+			}
+		}
+		return undefined;
+	}
+
+	private hasCollisionAt(): Location | undefined {
+		const grouped = _.groupBy(this.carts, c => c.moveOrder(this.track));
+		const collisions = _.filter(grouped, g => g.length > 1);
+		if (collisions.length > 0) {
+			return collisions[0][0].location;
+		}
+		return undefined;
 	}
 
 	private loadTrackAndCarts(filePath: string) {
