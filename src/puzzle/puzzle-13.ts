53c9db3c96efa9deafa41740d38203445434957b
Richard Pineo <Richard.E.Pineo@ge.com>
2018-12-16 15:01:51 -0800

    found test

--- src/puzzle/puzzle-13.ts
+++ src/puzzle/puzzle-13.ts
@@ -3,14 +3,18 @@ import Puzzle from './puzzle'
 import * as _ from 'lodash'
 
 enum Direction {
-	North = 'N',
-	South = 'S',
-	East = 'E',
-	West = 'W'
+	North = '^',
+	South = 'v',
+	East = '>',
+	West = '<'
 }
 
 class Location {
 	constructor(public x: number, public y: number) { }
+
+	moveOrder(track: Track) {
+		return this.x * track.dimX + this.y;
+	}
 }
 
 class Cart {
@@ -20,19 +24,23 @@ class Cart {
 	private turnCount = 0;
 
 	moveOrder(track: Track) {
-		return this.location.x * track.dimX + this.location.y;
+		return this.location.moveOrder(track);
 	}
 
 	turnRight() {
 		switch (this.facing) {
 			case Direction.North:
 				this.facing = Direction.East;
+				break;
 			case Direction.East:
 				this.facing = Direction.South;
+				break;
 			case Direction.South:
 				this.facing = Direction.West;
+				break;
 			case Direction.West:
 				this.facing = Direction.North;
+				break;
 		}
 	}
 
@@ -40,12 +48,16 @@ class Cart {
 		switch (this.facing) {
 			case Direction.North:
 				this.facing = Direction.West;
+				break;
 			case Direction.West:
 				this.facing = Direction.South;
+				break;
 			case Direction.South:
 				this.facing = Direction.East;
+				break;
 			case Direction.East:
 				this.facing = Direction.North;
+				break;
 		}
 	}
 
@@ -72,10 +84,20 @@ class Cart {
 		const part = track.getPart(this.location);
 		switch (part) {
 			case TrackPart.Clockwise:
-				this.turnRight();
+				if (this.facing === Direction.North || this.facing === Direction.South) {
+					this.turnLeft();
+				}
+				else {
+					this.turnRight();
+				}
 				break;
 			case TrackPart.Widdershins:
-				this.turnLeft();
+				if (this.facing === Direction.North || this.facing === Direction.South) {
+					this.turnRight();
+				}
+				else {
+					this.turnLeft();
+				}
 				break;
 			case TrackPart.Intersection: {
 				const action = this.turnCount % 3;
@@ -84,6 +106,7 @@ class Cart {
 						this.turnLeft();
 						break;
 					case 1:
+						break;
 					case 2:
 						this.turnRight();
 						break;
@@ -99,23 +122,24 @@ enum TrackPart {
 	NorthSouth = '|',
 	EastWest = '-',
 	Intersection = '+',
-	Clockwise = '/',
-	Widdershins = '\\'
+	Clockwise = '\\',
+	Widdershins = '/'
 }
 
 class Track {
-	constructor(public dimX: number, dimY: number) {
+	constructor(public dimX: number, public dimY: number) {
 	}
 
 	public getPart(location: Location) {
-		const found = this.parts.get(location);
+
+		const found = this.parts.get(location.moveOrder(this));
 		return found === undefined ? TrackPart.Empty : found;
 	}
 	public setPart(location: Location, part: TrackPart) {
-		this.parts.set(location, part);
+		this.parts.set(location.moveOrder(this), part);
 	}
 
-	private parts = new Map<Location, TrackPart>();
+	private parts = new Map<number, TrackPart>();
 }
 
 export default class Puzzle12 extends Puzzle {
@@ -146,7 +170,9 @@ export default class Puzzle12 extends Puzzle {
 	}
 
 	private runUntilCollision(): Location | undefined {
-		for (let tick = 0; tick < 10000; tick++) {
+		this.dumpTrack();
+		for (let tick = 0; tick < 100; tick++) {
+			console.log(`\n${tick} -----------`.white);
 			// Sort the carts by move order
 			this.carts.sort((a, b) => a.moveOrder(this.track) - b.moveOrder(this.track));
 			for (let i = 0; i < this.carts.length; i++) {
@@ -154,14 +180,35 @@ export default class Puzzle12 extends Puzzle {
 
 				c.move(this.track);
 
+				this.dumpTrack();
+
 				// check for collision
 				const collision = this.hasCollisionAt();
 				if (collision !== undefined) {
+					this.dumpTrack();
 					return collision;
 				}
 			}
 		}
 		return undefined;
+	}
+
+	private dumpTrack() {
+		console.log('');
+		for (let y = 0; y < this.track.dimY; y++) {
+			let line = '';
+			for (let x = 0; x < this.track.dimX; x++) {
+				const location = new Location(x, y);
+				const found = this.carts.find(c => c.moveOrder(this.track) == location.moveOrder(this.track));
+				if (found !== undefined) {
+					line += found.facing;
+				}
+				else {
+					line += this.track.getPart(location);
+				}
+			}
+			console.log(line.gray);
+		}
 	}
 
 	private hasCollisionAt(): Location | undefined {
@@ -214,9 +261,9 @@ export default class Puzzle12 extends Puzzle {
 	private toTrackPart(s: string): TrackPart {
 		switch (s) {
 			case "/":
-				return TrackPart.Clockwise;
-			case "\\":
 				return TrackPart.Widdershins;
+			case "\\":
+				return TrackPart.Clockwise;
 			case "-":
 			case ">":
 			case "<":
