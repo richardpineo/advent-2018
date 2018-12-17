4d6c714877c7a988bd2392c93845a0b20b45201e
Richard Pineo <Richard.E.Pineo@ge.com>
2018-12-17 00:56:29 -0800

    ugly but functional

--- src/puzzle/puzzle-14.ts
+++ src/puzzle/puzzle-14.ts
@@ -12,7 +12,7 @@ class Node {
 	public prev: Node;
 
 	pushBefore(value: number): Node {
-		const node = new Node(value, this, this.next);
+		const node = new Node(value, this, this.prev);
 		this.prev.next = node;
 		this.prev = node;
 		return node;
@@ -27,25 +27,30 @@ class Node {
 }
 
 // 14a: 2157138126 for 894512 recipes - what a score!
-// 14b:
+// 14b: 20365081 for 894501 - what a score!
 export default class Puzzle14 extends Puzzle {
 	constructor() {
 		super("14: Hot Choc");
 	}
 
 	solve() {
-
 		this.solveA(9);
 		this.solveA(5);
 		this.solveA(18);
 		this.solveA(2018);
 		this.solveA(894501);
-		this.solveB();
+
+		this.solveB('51589');
+		this.solveB('01245');
+		this.solveB('92510');
+		this.solveB('59414');
+		this.solveB('894501');
 	}
 
 	solveA(recipes: number) {
-		const atLeast = recipes + 10;
-		const start = this.solveFor(atLeast)
+		// Stop when we have enough
+		const stopIf = (start: Node, count: number) => recipes + 10 < count;
+		const start = this.solveFor(stopIf)
 
 		let score = '';
 		let scoreStart = start;
@@ -60,27 +65,66 @@ export default class Puzzle14 extends Puzzle {
 		console.log(`14a: ${score} for ${recipes} recipes - what a score!`);
 	}
 
-	solveB() {
+	solveB(sequence: string) {
+
+		const numbers = sequence.split('').map(n => parseInt(n));
+
+		let length = 0;
+		const stopIf = (start: Node, count: number) => {
+			/*
+			if (count % 10000 === 0) {
+				console.log(`${count} processed`)
+			}*/
+
+			let result = this.matches(start, numbers);
+			if (result === undefined) {
+				return false;
+			}
+			length = result;
+			return true;
+		}
+
+		this.solveFor(stopIf);
+		console.log(`14b: ${length} for ${sequence} - what a score!`);
 	}
 
-	solveFor(recipesNeeded: number): Node {
+	matches(start: Node, numbers: number[]): number | undefined {
+		let current = start.prev;
+		for (let i = numbers.length - 1; i >= 0; i--) {
+			if (current.value != numbers[i]) {
+				return undefined;
+			}
+			current = current.prev;
+		}
+		// See how long it is
+		let score = 1;
+		while (current != start) {
+			score++;
+			current = current.prev;
+		}
+		return score;
+	}
+
+	solveFor(shouldStop: (start: Node, count: number) => boolean): Node {
 		const start = new Node(3, undefined, undefined);
 		let elf1 = start;
 		let elf2 = start.push(7);
-		let numRecipes = 2;
 
 		this.dump(start, elf1, elf2);
 
-		for (let i = 0; numRecipes <= recipesNeeded; i++) {
+		for (let i = 0; !shouldStop(start, i); i++) {
 
 			// Add scores
 			const combined = elf1.value + elf2.value;
 
 			if (combined >= 10) {
 				start.pushBefore(1);
-				numRecipes++;
 			}
-			numRecipes++;
+
+			if (shouldStop(start, i)) {
+				return start;
+			}
+
 			start.pushBefore(combined % 10);
 
 			// Move elves
