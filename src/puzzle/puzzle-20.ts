1bcbc2b55f04a47de4c3e454b526c2b5f0846762
Richard Pineo <Richard.E.Pineo@ge.com>
2018-12-19 23:35:42 -0800

    tokenized

--- src/puzzle/puzzle-20.ts
+++ src/puzzle/puzzle-20.ts
@@ -3,6 +3,10 @@ import Puzzle from './puzzle'
 
 import * as _ from 'lodash'
 
+class Path {
+	options = new Array<Path | string>();
+}
+
 // 20a:
 // 20b:
 export default class Puzzle20 extends Puzzle {
@@ -19,7 +23,7 @@ export default class Puzzle20 extends Puzzle {
 			'./data/20-ex3',
 			'./data/20-ex4',
 			'./data/20-ex5',
-			'./data/20'
+			//	'./data/20'
 		]
 		allFiles.forEach(f => {
 			this.solveA(f);
@@ -33,12 +37,12 @@ export default class Puzzle20 extends Puzzle {
 			throw `Invalid regex: ${key}`;
 		}
 		console.log(`20a: Regex: ${_.truncate(key)}`.gray);
+		this.enableVerbose = key.length < 200;
 
 		// Generate all the paths that comprise the map
-		const paths = this.generatePaths(key);
+		const paths = this.flattenPaths(key);
 		this.verbose(`20a: ${paths.length} paths generated`);
 
-		this.enableVerbose = paths.length < 20;
 		this.verbose('Paths: ' + paths.join('\n'));
 
 		const numDoors = this.findMostDoors();
@@ -51,13 +55,61 @@ export default class Puzzle20 extends Puzzle {
 		}
 	}
 
-	generatePaths(key: string): string[] {
+	flattenPaths(key: string): string[] {
 		const letters = key.split('').slice(1);
 		letters.pop();
 
 		const tokens = this.tokenize(letters);
 		this.verbose('Tokens: ' + tokens.join(' '));
-		return tokens;
+
+		const path = this.generatePath(tokens);
+		this.dumpPath(path);
+		// flatten them
+
+		return [];
+	}
+
+	dumpPath(path: Path, depth = 0) {
+		if (!this.enableVerbose) {
+			return;
+		}
+
+		path.options.forEach(option => {
+			if (typeof option === 'string') {
+				this.verbose(`${depth}:  ${option}`);
+			}
+			else {
+				this.dumpPath(option, depth + 1);
+			}
+		})
+	}
+
+	generatePath(tokens: string[], state: { index: number } = { index: 0 }): Path {
+		const path = new Path();
+
+		for (let i = state.index; i < tokens.length; i++) {
+			const token = tokens[i];
+			switch (token) {
+				case '|':
+					// Don't do anything
+					break;
+				case '(':
+					let newState = { index: i + 1 };
+					const nested = this.generatePath(tokens, newState);
+					path.options.push(nested);
+					i = newState.index;
+					break;
+				case ')':
+					state.index = i;
+					return path;
+				default:
+					// must be a word
+					path.options.push(token);
+					break;
+			}
+		}
+		state.index = tokens.length;
+		return path;
 	}
 
 	tokenize(letters: string[]): string[] {
